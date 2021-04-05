6d651479d18958f1c3d054c6c27add23651f5911
Yaroslav Mazharov <idragon91@gmail.com>
2021-04-05 16:49:34 +0300

    FirstCommit

---
+++ Product1/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Product1
+//
+//  Created by Admin on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myWorkAria()
+//*********************************************
+        let num = 50
+        num == 45 ? print("Ono"): print("ne ono")
+//**********************************************
+        
+        let result = 2 > 1
+        print(result)
+        
+        let someText:String = "some text"
+        let numb:Int = 7
+        let withDot:Double = 2.745
+        print(someText, numb, withDot)
+        
+        
+        
+//**********************************************
+        
+        
+        
+    }
+    
+    func myWorkAria() {
+        print("Huilo")
+        
+        
+    }
+    
+}
+
